ee49ba39330f74d1880fcbc06b83ae3bd2bcd3a2
fsiraj <fsiraj@umich.edu>
2022-11-08 23:09:08 -0500

    v2.0

--- gloveapp/App.js
+++ gloveapp/App.js
@@ -1,117 +1,226 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, Button, PermissionsAndroid } from 'react-native';
+import { React, useState } from 'react';
+import { BleManager, Device } from 'react-native-ble-plx';
+import { Slider } from '@miblanchard/react-native-slider';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+// const BLTManager = new BleManager();
+// const SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
+// const MESSAGE_UUID = '6d68efe5-04b6-4a85-abc4-c2670b7bf7fd';
+// const BOX_UUID = 'f27b53ad-c63d-49a0-8c0f-9f297e6cc520';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function StringToBool(input) {
+  if (input == '1') {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
+function BoolToString(input) {
+  if (input == true) {
+    return '1';
+  } else {
+    return '0';
+  }
+}
+
+export default function App() {
+  //Is connect page displayed
+  const connectPage = useState(true)
+  //Is a device connected?
+  const [isConnected, setIsConnected] = useState(false);
+  //What device is connected?
+  const [connectedDevice, setConnectedDevice] = useState();
+
+  const [vibrationValue, setVibrationValue] = useState(50);
+
+  // Scans availbale BLT Devices and then call connectDevice
+  async function scanDevices() {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Permission Localisation Bluetooth',
+        message: 'Requirement for Bluetooth',
+        buttonNeutral: 'Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    ).then(answere => {
+      console.log('scanning');
+      // display the Activityindicator
+
+      BLTManager.startDeviceScan(null, null, (error, scannedDevice) => {
+        if (error) {
+          console.warn(error);
+        }
+
+        if (scannedDevice && scannedDevice.name == 'BLEExample') {
+          BLTManager.stopDeviceScan();
+          connectDevice(scannedDevice);
+        }
+      });
+
+      // stop scanning devices after 5 seconds
+      setTimeout(() => {
+        BLTManager.stopDeviceScan();
+      }, 5000);
+    });
+  }
+
+  // handle the device disconnection (poorly)
+  async function disconnectDevice() {
+    console.log('Disconnecting start');
+
+    if (connectedDevice != null) {
+      const isDeviceConnected = await connectedDevice.isConnected();
+      if (isDeviceConnected) {
+        BLTManager.cancelTransaction('messagetransaction');
+        BLTManager.cancelTransaction('nightmodetransaction');
+
+        BLTManager.cancelDeviceConnection(connectedDevice.id).then(() =>
+          console.log('DC completed'),
+        );
+      }
+
+      const connectionStatus = await connectedDevice.isConnected();
+      if (!connectionStatus) {
+        setIsConnected(false);
+      }
+    }
+  }
+
+  //Connect the device and start monitoring characteristics
+  async function connectDevice(device) {
+    console.log('connecting to Device:', device.name);
+
+    device
+      .connect()
+      .then(device => {
+        setConnectedDevice(device);
+        setIsConnected(true);
+        return device.discoverAllServicesAndCharacteristics();
+      })
+      .then(device => {
+        //  Set what to do when DC is detected
+        BLTManager.onDeviceDisconnected(device.id, (error, device) => {
+          console.log('Device DC');
+          setIsConnected(false);
+        });
+
+        //Read inital values
+
+        //Message
+        device
+          .readCharacteristicForService(SERVICE_UUID, MESSAGE_UUID)
+          .then(valenc => {
+            setMessage(base64.decode(valenc?.value));
+          });
+
+        //SliderValue
+        device
+          .readCharacteristicForService(SERVICE_UUID, BOX_UUID)
+          .then(valenc => {
+            setBoxValue(StringToBool(base64.decode(valenc?.value)));
+          });
+
+        //monitor values and tell what to do when receiving an update
+
+        //Message
+        device.monitorCharacteristicForService(
+          SERVICE_UUID,
+          MESSAGE_UUID,
+          (error, characteristic) => {
+            if (characteristic?.value != null) {
+              setMessage(base64.decode(characteristic?.value));
+              console.log(
+                'Message update received: ',
+                base64.decode(characteristic?.value),
+              );
+            }
           },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
+          'messagetransaction',
+        );
+
+        //BoxValue
+        device.monitorCharacteristicForService(
+          SERVICE_UUID,
+          BOX_UUID,
+          (error, characteristic) => {
+            if (characteristic?.value != null) {
+              setBoxValue(StringToBool(base64.decode(characteristic?.value)));
+              console.log(
+                'Box Value update received: ',
+                base64.decode(characteristic?.value),
+              );
+            }
           },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+          'boxtransaction',
+        );
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+        console.log('Connection established');
+      });
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+
+  }
+
+  //Function to send data to ESP32
+  async function sendSliderValue(value) {
+    BLTManager.writeCharacteristicWithResponseForDevice(
+      connectedDevice?.id,
+      SERVICE_UUID,
+      BOX_UUID,
+      base64.encode(value.toString()),
+    ).then(characteristic => {
+      console.log('SliderValue changed to :', base64.decode(characteristic.value));
+    });
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <View style={styles.container}>
+      <View style={styles.rowView}>
+        <TouchableOpacity style={{ width: 120 }}>
+          {!isConnected ? (
+            <Button
+              title="Connect"
+              onPress={() => {
+                scanDevices();
+              }}
+              disabled={false}
+            />
+          ) : (
+            <Button
+              title="Disonnect"
+              onPress={() => {
+                disconnectDevice();
+              }}
+              disabled={false}
+            />
+          )}
+        </TouchableOpacity>
+        <View style={styles.container}>
+          <Slider
+            minimumValue={0}
+            maximumValue={100}
+            step={10}
+            value={vibrationValue}
+            onValueChange={value => setVibrationValue(value)}
+          />
+          <Text> Vibration Level: {vibrationValue}</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+      </View>
+    </View>
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  );
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    textAlign: 'center'
   },
 });
-
-export default App;
--- gloveapp/Slider.js
+++ gloveapp/Slider.js
@@ -1,50 +1,44 @@
-/* eslint-disable prettier/prettier */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Slider } from '@miblanchard/react-native-slider';
 
-'use strict';
- 
-var React = require('react');
-var Slider = require('react-native-slider');
+class VibrationSlider extends React.Component {
 
-var {
-  AppRegistry,
-  StyleSheet,
-  View,
-  Text,
-} = require('react-native');
- 
-var VibrationSlider = React.createClass({
-  getInitialState() {
-    return {
-      value: 5, // put default here
+    state = {
+        value: 50,
     };
-  },
 
-  valueChange(value) {
-    this.setState({value});
-    // put code to communicate with the adafruit
+    valueChange(value) {
+        this.setState({ value });
+        // put code to communicate with the adafruit
+    }
 
-  },
- 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Slider
-          value={this.state.value}
-          onValueChange={(value) => this.valueChange({value})} />
-        <Text>Value: {this.state.value}</Text>
-      </View>
-    );
-  },
-});
- 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginLeft: 10,
-    marginRight: 10,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-});
- 
+    render() {
+        return (
+            <View style={styles.container}>
+                <Slider
+                    minimumValue={0}
+                    maximumValue={100}
+                    step={10}
+                    value={this.state.value}
+                    onValueChange={value => this.valueChange(value)}
+                />
+                <Text> Vibration Level: {this.state.value}</Text>
+            </View>
+        );
+    }
+}
+
 export default VibrationSlider;
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginLeft: 10,
+        marginRight: 10,
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+});
